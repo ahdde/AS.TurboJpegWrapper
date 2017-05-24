69a6e502406c86291be9454dae84da220a944180
Bianco Veigel <bianco.veigel@ahd.de>
2017-05-24 20:27:27 +0200

    add mozjpeg

---
+++ build_mozjpeg.bat
@@ -0,0 +1,15 @@
+mkdir mozjpeg\bin\win32
+mkdir mozjpeg\bin\win64
+
+cd mozjpeg\bin\win32
+cmake -G "Visual Studio 14 2015" ..\..
+msbuild /P:Configuration=MinSizeRel ALL_BUILD.vcxproj
+cd ..\..\..
+
+cd mozjpeg\bin\win64
+cmake -G "Visual Studio 14 2015 Win64" ..\..
+msbuild /P:Configuration=MinSizeRel ALL_BUILD.vcxproj
+cd ..\..\..
+
+copy mozjpeg\bin\win32\MinSizeRel\turbojpeg.dll LibJpegWrapper\costura32\turbojpeg.dll
+copy mozjpeg\bin\win64\MinSizeRel\turbojpeg.dll LibJpegWrapper\costura64\turbojpeg.dll
