13033bcc24d0e393094c20c4d444c7219f6f52cb
Bianco Veigel <bianco.veigel@ahd.de>
2018-04-09 11:19:15 +0200

    update mozjpeg binaries

--- build_mozjpeg.bat
+++ build_mozjpeg.bat
@@ -2,21 +2,20 @@ cd mozjpeg
 git reset --hard
 git clean -f -d
 cd ..
-git apply turbojpeg.patch
 git apply jcparam.patch
 
 mkdir mozjpeg\bin\win32
 mkdir mozjpeg\bin\win64
 
 cd mozjpeg\bin\win32
-cmake -G "Visual Studio 14 2015" ..\..
+cmake -G "Visual Studio 15 2017" ..\..
 msbuild /P:Configuration=MinSizeRel ALL_BUILD.vcxproj
 cd ..\..\..
 
 cd mozjpeg\bin\win64
-cmake -G "Visual Studio 14 2015 Win64" ..\..
+cmake -G "Visual Studio 15 2017 Win64" ..\..
 msbuild /P:Configuration=MinSizeRel ALL_BUILD.vcxproj
 cd ..\..\..
 
-copy mozjpeg\bin\win32\MinSizeRel\turbojpeg.dll LibJpegWrapper\costura32\turbojpeg.dll
-copy mozjpeg\bin\win64\MinSizeRel\turbojpeg.dll LibJpegWrapper\costura64\turbojpeg.dll
+copy /Y mozjpeg\bin\win32\MinSizeRel\turbojpeg.dll LibJpegWrapper\costura32\turbojpeg.dll
+copy /Y mozjpeg\bin\win64\MinSizeRel\turbojpeg.dll LibJpegWrapper\costura64\turbojpeg.dll
